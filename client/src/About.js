d16d7eafe9478e18ff9bc3119eefd4524b3538d0
Morgan Byrne <79115095+fortfreedom3@users.noreply.github.com>
2022-02-04 12:50:27 -0500

    updating About css

---
+++ client/src/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function About() {
+
+    const mystyle = {
+        color: "a35748",
+        backgroundColor: "DodgerBlue",
+        padding: "10px",
+        fontFamily: "Arial"
+      };
+
+    return (
+        <div>
+            <h1 style={{ color: "#a35748" }}>About</h1>
+            <p>Are you having so much fun learning how to use CSS within our React components?</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ client/src/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Contact () {
+
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p>Reach out if you have any questions.</p>
+        </div>
+    )
+
+}
+
+export default Contact;
