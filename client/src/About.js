f499d9298637010addda299b06f870285a3778b2
Morgan Byrne <79115095+fortfreedom3@users.noreply.github.com>
2022-02-04 21:34:29 -0500

    added some css, about component

--- client/src/About.js
+++ client/src/About.js
@@ -1,20 +1,61 @@
 import React from "react";
 
+
+
+
 function About() {
-
-    const mystyle = {
-        color: "a35748",
-        backgroundColor: "DodgerBlue",
-        padding: "10px",
-        fontFamily: "Arial"
-      };
-
     return (
         <div>
-            <h1 style={{ color: "#a35748" }}>About</h1>
+            <h1 className="myStyle" >About</h1>
             <p>Are you having so much fun learning how to use CSS within our React components?</p>
+            <button type="button" className="btn btn-primary" variant="outline-primary">Primary</button>
+            
+            <div class="container">
+            <div class="row">
+                <div class="col-xs-6 col-sm-8 col-lg-10">
+                    <img src="./client/src/images/wood-malas.jpg" />
+                </div>
+                <div class="col-xs-6 col-sm-8 col-lg-10"> 
+                    <h2>Welcome</h2>
+
+                    <p>Now that you are here take a deep breath in and a long exhale.<p/>
+
+                    <p>
+                    A mala, or japa mala, is a string of meditation beads. it is similar to a Christian rosary and it is used to help you lengthen your breath or to repeat a mantra, intention, or a prayer as you move your fingers over each bead. You can wear a mala to help you feel more grounded and balanced, or simply because you think it looks rad! 
+                    </p>
+
+                    <p>
+                    Whether you are looking for a mala for yourself or as a gift, take a moment to close your eyes and think about how you want to feel (or how you want the gift recipient to feel). Perhaps you seek balance, grounding, loving, or calming vibes. think about that feeling you want to cultivate more of. Then scroll through to see what pops out for you. when choosing a mala I believe we should go with our first instinct and don't overthink it. Usually the stones you are first drawn to have grounding or balancing properties you may want to bring into your meditation practice. 
+                    </p>
+
+                    <p>
+                    I love to help people create custom and uniquely personalized malas. Feel free to send me a message if you want to learn more about how we can make a unique combination of gemstones to help you raise your vibe in your meditation practice. 
+                    </p>
+
+                    <p>
+                    If you have questions about how to use a mala, how to care for your mala, or about personalizing a mala please see the FAQ section. 
+                    </p></p>
+                </div>
+                
+            </div>
+
+            <div class="row">
+                <div class="col-xs-7 col-sm-6 col-lg-8">.col-xs-7 .col-sm-6 .col-lg-8</div>
+                <div class="col-xs-5 col-sm-6 col-lg-4">.col-xs-5 .col-sm-6 .col-lg-4</div>
+            </div>
+
+            <div class="row">
+                <div class="col-xs-6 col-sm-8 col-lg-10">.col-xs-6 .col-sm-8 .col-lg-10</div>
+                <div class="col-xs-6 col-sm-4 col-lg-2">.col-xs-6 .col-sm-4 .col-lg-2</div>
+            </div>
+            </div>
+
+
+
+
         </div>
     )
 }
 
 export default About;
+
--- client/src/Contact.js
+++ client/src/Contact.js
@@ -1,14 +1,13 @@
 import React from "react";
 
-function Contact () {
-
+function Contact() {
     return (
         <div>
-            <h1>Contact</h1>
+            <h1 className="myStyle">Contact</h1>
             <p>Reach out if you have any questions.</p>
+            <button>Contact</button>
         </div>
     )
-
 }
 
 export default Contact;
