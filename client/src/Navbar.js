cc861a90afec1e9cb04864bcb5684a44e6c55122
Morgan Byrne <79115095+fortfreedom3@users.noreply.github.com>
2022-02-08 10:13:33 -0500

    added logo to navbar

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,24 +1,25 @@
 import React from "react";
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import logo_wg_rectangle from './images/logo-rectangle-2.png';
 
 function Header() {
   return (
 <Navbar>
 <Container>
   <div className="nav-div">
-  <Navbar.Brand href="/"> Brand Name Here {" "}
+  <Navbar.Brand href="/"><img src={logo_wg_rectangle} alt="moon icon and morgan moonchild logo in gold" className="nav-logo" />
   </Navbar.Brand>
   </div>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <div className="drop-down">
 
-  <div class="container" className="nav-buttons">
+  <div className="container" className="nav-buttons">
     <Nav className="me-auto">
       <Nav.Link className= "nav-but" href="/">Home</Nav.Link>
       <Nav.Link className= "nav-but" href="/about">About</Nav.Link>
       <Nav.Link className= "nav-but" href="/contact">Contact</Nav.Link>
-
+      <Nav.Link className= "nav-but" href="/shop">Shop Now</Nav.Link>
     </Nav>
     </div>
     </div>
