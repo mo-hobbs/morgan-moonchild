f1d2d14e54037cee19f488d7f90f7d97b115ac58
Morgan Byrne <79115095+fortfreedom3@users.noreply.github.com>
2022-02-07 19:55:48 -0500

    product data fetch

--- client/src/ProductCard.js
+++ client/src/ProductCard.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-function ProductCard() {
+function ProductCard({ p }) {
+
+    const { product_title, description, price, image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, status } = p;
+
     return (
         <div>
-            <h3>ProductCards Go Here</h3>
+            <h4>{product_title}</h4>
+            <p>{price}</p>
+            <img src={image1} />
         </div>
     )
 }
