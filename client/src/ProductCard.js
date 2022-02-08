04c3321d7d1a28e624c5ccc0c6c262ca9f70c9ab
Morgan Byrne <79115095+fortfreedom3@users.noreply.github.com>
2022-02-08 12:36:39 -0500

    one long column of product cards

--- client/src/ProductCard.js
+++ client/src/ProductCard.js
@@ -1,15 +1,29 @@
 import React from "react";
+import { Row, Col, Card } from 'react-bootstrap';
 
 function ProductCard({ p }) {
 
     const { product_title, description, price, image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, status } = p;
+    console.log(image1)
 
     return (
-        <div>
-            <h4>{product_title}</h4>
-            <p>{price}</p>
-            <img src={image1} />
-        </div>
+        <Row xs={1} md={3} className="g-4">
+            {Array.from({ length: 1 }).map((_, idx) => (
+
+        
+            <Col>
+                <Row>
+                <Card>
+                <Card.Img variant="top" src={image1} />
+                <Card.Body>
+                    <Card.Title>{product_title}</Card.Title>
+                    <Card.Text>{price}</Card.Text>
+                </Card.Body>
+                </Card>
+            </Row>
+            </Col>
+          ))}
+        </Row>
     )
 }
 
