3a3206d4423fe63cde0959408a62647184265ed1
Morgan Byrne <79115095+fortfreedom3@users.noreply.github.com>
2022-02-10 09:21:24 -0500

    product images

--- client/src/ProductCard.js
+++ client/src/ProductCard.js
@@ -1,30 +1,14 @@
 import React from "react";
 import moon_logo_gold from "./images/moon-logo-gold.png";
 import { Row, Col, Card, Container } from 'react-bootstrap';
-import Product from "./Product";
+
 
 function ProductCard({ p }) {
 
     const { product_title, description, price, image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, status } = p;
 
-
-    function renderImage(imageToString) {
-        console.log(imageToString.image1.slice(9, -11));
-        //this needs to return a variable that can be put in a src string
-        const imageStr = imageToString.image1.slice(9, -11);
-        return (
-            <div>
-                {/* <img src={imageStr}/> */}
-            </div>
-        )
-    }
-
-
     return (
         <Container>
-            <Row>
-            {/* {renderImage({image1})} */}
-            </Row>
             <Row xs={1} md={2}>
             <Col md={{ span: 4, offset: 4 }}>
             <br />
