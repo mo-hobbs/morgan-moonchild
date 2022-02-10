0dbba19a9c5a1bb5b4fc4b81975df28b0cdbed82
Morgan Byrne <79115095+fortfreedom3@users.noreply.github.com>
2022-02-10 17:13:58 -0500

    contact form updated

--- client/src/Contact.js
+++ client/src/Contact.js
@@ -1,12 +1,31 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Form, Button, Container } from "react-bootstrap";
 
 function Contact() {
     return (
         <Container >
-            <h1>Contact</h1>
-            <p>Reach out if you have any questions.</p>
-            <button>Contact</button>
+        <Form>
+            <h2>Contact</h2>
+            <p>Happy to help and answer any questions</p>
+            <Form.Group className="mb-3" >
+                <Form.Control type="text" placeholder="Your name" />
+                </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+
+                <Form.Control type="email" placeholder="Email" />
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" >
+                <Form.Control type="text" className="form-message" placeholder="Message" />
+            </Form.Group>
+
+            <Button variant="dark" type="submit">
+                Submit
+            </Button>
+        </Form>
         </Container>
     )
 }
