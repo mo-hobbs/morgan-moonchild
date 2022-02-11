f1e83f623374fca2fe1493f18ab83d3be3e4974f
Morgan Byrne <79115095+fortfreedom3@users.noreply.github.com>
2022-02-11 07:40:06 -0500

    Nav drop down

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Container, Nav, Button, Row, Col } from 'react-bootstrap';
+import { Navbar, NavDropdown, Container, Nav, Button, Row, Col } from 'react-bootstrap';
 import logo_wg_rectangle from './images/logo-rectangle-2.png';
 
 function Header({ onLogout, user }) {
@@ -9,6 +9,9 @@ function Header({ onLogout, user }) {
     }).then(() => onLogout());
   }
 
+  const navStyle = {
+    color: "black"
+  }
 
   return (
     <Navbar >
@@ -16,7 +19,7 @@ function Header({ onLogout, user }) {
       <Navbar.Brand href="/"><img src={logo_wg_rectangle} alt="moon icon and morgan moonchild logo in gold" className="nav-logo" />
         </Navbar.Brand>
 
-      <Navbar.Collapse id="responsive-navbar-nav">
+      {/* <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="me-auto">
           <Nav.Link className= "nav-but" href="/">Home</Nav.Link>
           <Nav.Link className= "nav-but" href="/about">About</Nav.Link>
@@ -24,9 +27,16 @@ function Header({ onLogout, user }) {
           <Nav.Link className= "nav-but" href="/login" onClick={handleLogout}>{user ? "Logout" : "Login" }</Nav.Link>
           <Nav.Link className= "nav-but" href="/shop">Shop</Nav.Link>
         </Nav>
+      </Navbar.Collapse> */}
 
-
-      </Navbar.Collapse>
+      <NavDropdown title="Menu" href="/" style={navStyle} id="collasible-nav-dropdown">
+        <NavDropdown.Item href="/">Home</NavDropdown.Item>
+        <NavDropdown.Item href="/shop">Shop</NavDropdown.Item>
+        <NavDropdown.Item href="/about">About</NavDropdown.Item>
+        <NavDropdown.Item href="/contact">Contact</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="/login" onClick={handleLogout}>{user ? "Logout" : "Login" }</NavDropdown.Item>
+      </NavDropdown>
         </Container>
     </Navbar>
 
