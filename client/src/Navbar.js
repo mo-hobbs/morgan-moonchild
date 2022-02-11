b612d7a806fe4a37e18cfb0dd3f0bd27340b182a
Morgan Byrne <79115095+fortfreedom3@users.noreply.github.com>
2022-02-11 08:10:03 -0500

    collapse on select navbar

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -9,37 +9,25 @@ function Header({ onLogout, user }) {
     }).then(() => onLogout());
   }
 
-  const navStyle = {
-    color: "black"
-  }
-
   return (
-    <Navbar >
+    <Navbar collapseOnSelect expand="md" >
       <Container>
-      <Navbar.Brand href="/"><img src={logo_wg_rectangle} alt="moon icon and morgan moonchild logo in gold" className="nav-logo" />
+        <Navbar.Brand href="/"><img src={logo_wg_rectangle} alt="moon icon and morgan moonchild logo in gold" className="nav-logo" />
         </Navbar.Brand>
 
-      {/* <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="me-auto">
-          <Nav.Link className= "nav-but" href="/">Home</Nav.Link>
-          <Nav.Link className= "nav-but" href="/about">About</Nav.Link>
-          <Nav.Link className= "nav-but" href="/contact">Contact</Nav.Link>
-          <Nav.Link className= "nav-but" href="/login" onClick={handleLogout}>{user ? "Logout" : "Login" }</Nav.Link>
-          <Nav.Link className= "nav-but" href="/shop">Shop</Nav.Link>
-        </Nav>
-      </Navbar.Collapse> */}
-
-      <NavDropdown title="Menu" href="/" style={navStyle} id="collasible-nav-dropdown">
-        <NavDropdown.Item href="/">Home</NavDropdown.Item>
-        <NavDropdown.Item href="/shop">Shop</NavDropdown.Item>
-        <NavDropdown.Item href="/about">About</NavDropdown.Item>
-        <NavDropdown.Item href="/contact">Contact</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="/login" onClick={handleLogout}>{user ? "Logout" : "Login" }</NavDropdown.Item>
-      </NavDropdown>
-        </Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+            <Nav.Link  href="/">Home</Nav.Link>
+            <Nav.Link href="/shop">Shop</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          <NavDropdown.Divider />
+          <Nav.Link href="/login" onClick={handleLogout}>{user ? "Logout" : "Login" }</Nav.Link>
+          </Nav>
+          </Navbar.Collapse>
+      </Container>
     </Navbar>
-
   )
 }
 
