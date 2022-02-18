615a5bddc53792d3b397d7585797c1192c331810
Morgan Byrne <79115095+fortfreedom3@users.noreply.github.com>
2022-02-17 21:43:59 -0500

    Update Footer.js

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -49,7 +49,7 @@ function Footer() {
         </Col>
       </Row>
       <Row className="text-center" style={{color: "#968F71"}}>
-        <p><small>© 2020 Morgan Moonchild Inc.</small></p>
+        <p><small>© 2022 Morgan Moonchild Inc.</small></p>
       </Row>
     </Container>
   );
